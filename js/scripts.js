d5539bce514b11332ed192653af7edce677ee173
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-07-18 15:04:37 -0500

    Added ClientJS allowing device detection

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $( document ).ready(function() {
 
+	var client = new ClientJS(); // Create A New Client Object
 
+	var OS = client.getOS(); // Get OS Version
 
+	console.log( OS );
 
+	var osVersion = client.getOSVersion(); // Get OS Version
 
+	console.log( osVersion );
 
+	var browser = client.getBrowser(); // Get Browser
+
+	console.log( browser );
 
 
 });
